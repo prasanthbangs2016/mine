1f31cddb2cb07530e9d72b913a95a1b36cc2919f
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-08-15 00:14:24 +0530

    added code

--- 2-Roboshop-components-shell/components/1-frontend.sh
+++ 2-Roboshop-components-shell/components/1-frontend.sh
@@ -6,26 +6,6 @@ HEAD "Installing Nginx"
 yum install nginx -y &>>/tmp/roboshop.log
 #Prints msg in date format
 #echo -e "[\e[1;34mINFO\e[0m] [\e[1;35m${COMPONENT}\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] $1"
+STAT $?
 
 
-
-HEAD "Start Nginx"
-systemctl enable nginx
-systemctl start nginx
-
-HEAD "Downloading frontend from repo"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/static/* .
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
-
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/static/* .
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
-
-systemctl restart nginx
--- 2-Roboshop-components-shell/components/common.sh
+++ 2-Roboshop-components-shell/components/common.sh
@@ -3,3 +3,13 @@ HEAD() {
     echo -n -e "\e[32mdone\e[0m \t\t ...."
 
 }
+
+STAT() {
+    #first argument of function is =0
+    if [ $1 -eq 0 ]; then
+      echo -e "\e[1;31m done\e[0m"
+    else
+      echo -e "\e[1;31m fail\e[0m"
+
+    fi
+}
