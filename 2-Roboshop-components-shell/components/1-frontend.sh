9bada7239ebf3261e6aabe42d811b4935ab51c69
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-08-15 00:19:49 +0530

    added code

--- 2-Roboshop-components-shell/components/1-frontend.sh
+++ 2-Roboshop-components-shell/components/1-frontend.sh
@@ -3,7 +3,7 @@ source components/common.sh
 
 HEAD "Installing Nginx"
 #echo -n "Installing NGINX   ..."
-yum install nginx -y &>>/tmp/roboshop.log
+yum install nginxs -y &>>/tmp/roboshop.log
 #Prints msg in date format
 #echo -e "[\e[1;34mINFO\e[0m] [\e[1;35m${COMPONENT}\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] $1"
 STAT $?
