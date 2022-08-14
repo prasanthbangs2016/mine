5c02d38ff65eab42d520c9d80bedeedd32515b34
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-08-15 00:18:53 +0530

    added code

--- 2-Roboshop-components-shell/components/common.sh
+++ 2-Roboshop-components-shell/components/common.sh
@@ -1,5 +1,6 @@
 HEAD() {
     #echo -e "\e[32mdone\e[0m"
+    #funtion argument passed in double quotes ex: Installing nginx
     echo -n -e "\e[32m$1\e[0m \t\t ...."
 
 }
@@ -10,6 +11,10 @@ STAT() {
       echo -e "\e[1;31m done\e[0m"
     else
       echo -e "\e[1;31m fail\e[0m"
+      #if it is failed telling logs to check
+      echo -e "\t \e[1;33m Check logs for more detail.... Logfile: /tmp/roboshop\e[0m"
+      #stopping when failed.
+      exit 1
 
     fi
 }
