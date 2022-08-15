66b4032ecd14685859cb7285556b295441f52b3c
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-08-15 13:28:48 +0530

    added code

--- 2-Roboshop-components-shell/instance-launch.sh
+++ 2-Roboshop-components-shell/instance-launch.sh
@@ -9,11 +9,42 @@ if [ -z "${COMPONENT}" ]; then
   exit 1
 fi
 
+#$ bash instance-launch.sh
+#o/p: Component name is needed
+
+
 LaunchTemplateId=lt-0b86176ba30da7a45
 Version=1
+
+echo "Validating ${COMPONENT} instance is already there"
+#filter the instance by tag
+#also i want to tell whether instance is running/terminated/stopped as well
+#Reservations[] : telling its a list
+#Instances[] : telling its a list
+#xargs -n1 : remove that quote ("terminated") : o/p: terminated
+
+#aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name
+
+INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | xargs -n1)
+
+if [ "${INSTANCE_STATE}" = "runnung" ]; then
+  echo "${COMPONENT} instance is already exist"
+  #as instance running exit
+  exit 0
+fi
+
+if [ "${INSTANCE_STATE}" = "stopped" ]; then
+  echo "${COMPONENT} instance is already exist and stopped..!"
+  #as instance running exit
+  exit 0
+fi
+
 
 #launch ec2 using launch template and specify tags to instance
 #aws ec2 run-instances --launch-template LaunchTemplateId=lt-0b86176ba30da7a45,Version=1 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=frontend}]'
 
 #since with single quotes we cannot access the variable hence double quotes
-aws ec2 run-instances --launch-template LaunchTemplateId=${LaunchTemplateId},Version=${Version} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
+#as output coming json format,whole output we're giving to "JQ" and that stops and takes us to cmdline
+aws ec2 run-instances --launch-template LaunchTemplateId=${LaunchTemplateId},Version=${Version} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+
+
