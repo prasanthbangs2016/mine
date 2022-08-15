b1d8368a9b8d611a1bff23d0df5bfd13adccf94c
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-08-16 01:23:10 +0530

    added code

---
+++ 2-Roboshop-components-shell/components/5-user.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+source components/common.sh
+#removing log file for every time to have latest run log
+rm -rf /tmp/roboshop.log
+set-hostname user
+
+HEAD "Installing node"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/rboshop.log
+yum install nodejs -y &>>/tmp/rboshop.log
+
+STAT $?
+
+HEAD "Add roboshop user"
+#checking roboshop user id is exit or no
+id roboshop &>>/tmp/roboshop.log
+if [ $? -eq 0 ]; then
+  echo "Roboshop user is already exist so skipping"  &>>/tmp/roboshop.log
+  STAT $?
+else
+  useradd roboshop &>>/tmp/roboshop.log
+  STAT $?
+fi 
+
+HEAD "Download user code from github"
+curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Unzip downloaded user code"
+cd /home/roboshop
+rm -rf *
+unzip -o /tmp/user.zip &>>/tmp/roboshop.log
+mv user-main user
+cd /home/roboshop/user
+npm install --unsafe-perm &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Fix app permissions\t"
+#as we ran using sudo it will have root and its requires roboshop
+chown roboshop:roboshop /home/roboshop -R
+STAT $?
+
+HEAD "Update mongodb server in catalogue"
+#sudo su -
+#cat /home/roboshop/catalogue/systemd.service
+sed -i -e 's/MONGO_DNSNAME/dev-mongodb.roboshop.ppk/' /home/roboshop/catalogue/systemd.service &>>/tmp/roboshop.log
+sed -i -e 's/MONGO_ENDPOINT/dev-redis.roboshop.ppk/' /home/roboshop/catalogue/systemd.service &>>/tmp/roboshop.log
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+STAT $?
+
+HEAD "Starting systemD of user"
+systemctl enable user && systemctl daemon-reload >>/tmp/roboshop.log && systemctl start user >>/tmp/roboshop.log
+STAT $?
