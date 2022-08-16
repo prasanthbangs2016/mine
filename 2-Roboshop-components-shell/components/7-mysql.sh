c15feeccec02b7cf8faded6417fec8ce1e8986b1
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-08-17 00:43:06 +0530

    added code

---
+++ 2-Roboshop-components-shell/components/7-mysql.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+COMPONENT=mysql
+source components/common.sh
+rm -rf /tmp/roboshop.log
+
+if [ -z "$MYSQL_PASSWORD" ]; then
+  echo -e "\e[33m env variable MYSQL_PASSWORD is missing \e[0m"
+  exit 1
+fi
+
+HEAD "Installing Mysql"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>/tmp/roboshop.log
+yum install mysql-community-server -y &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "start mysql service"
+systemctl enable mysqld &>>/tmp/roboshop.log
+systemctl start mysqld &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "change mysql password"
+#DEFAULT_PASSWORD=$(sudo grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+echo "show databases;" | mysql -uroot -p$MYSQL_PASSWORD &>>/tmp/roboshop.log
+#if above commnand password is wrong we're changing the password
+if [ $? -ne 0 ]; then
+  echo Changing Default Password
+  DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+  echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWORD';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD} &>>/tmp/roboshop.log
+  STAT $?
+fi
+
+HEAD "checking plugin is available or not if not available removing it"
+echo "show plugins;" | mysql -uroot -p$MYSQL_PASSWORD 2>&1 | grep validate_password &>>/tmp/roboshop.log
+#checking plugin is available or not if not available removing it
+if [ $? -eq 0 ]; then
+  echo Remove Password Validate Plugin
+  echo "uninstall plugin validate_password;" | mysql -uroot -p$MYSQL_PASSWORD &>>/tmp/roboshop.log
+  STAT $?
+fi
+
+HEAD "Downloading mysql files from repo and load schemas"
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>/tmp/roboshop.log
+cd /tmp
+unzip -o mysql.zip &>>/tmp/roboshop.log
+cd mysql-main
+mysql -u root -pRoboShop@1 <shipping.sql &>>/tmp/roboshop.log
+STAT $?
+
+
+
