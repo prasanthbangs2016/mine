28d3b1ef272c3655599c963dad8f429f5bd52aab
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-08-17 01:15:38 +0530

    added code

--- 2-Roboshop-components-shell/components/7-mysql.sh
+++ 2-Roboshop-components-shell/components/7-mysql.sh
@@ -42,13 +42,13 @@ STAT $?
 #mysql -uroot -p"${DEFAULT_PASSWORD}" &>>/tmp/roboshop.log
 #STAT $?
 
-HEAD "Downloading mysql files from repo and load schemas"
-curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>/tmp/roboshop.log
-cd /tmp
-unzip -o mysql.zip &>>/tmp/roboshop.log
-cd mysql-main
-mysql -u root -pRoboShop@1 <shipping.sql &>>/tmp/roboshop.log
-STAT $?
+#HEAD "Downloading mysql files from repo and load schemas"
+#curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>/tmp/roboshop.log
+#cd /tmp
+#unzip -o mysql.zip &>>/tmp/roboshop.log
+#cd mysql-main
+#mysql -u root -pRoboShop@1 <shipping.sql &>>/tmp/roboshop.log
+#STAT $?
 
 
 
